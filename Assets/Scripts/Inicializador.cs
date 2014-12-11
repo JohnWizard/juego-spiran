538698422bbd5d09a00459f141b71009d7036fc0
pvlz <pavm.met@gmail.com>
2014-12-11 17:11:41 -0600

    Agregué un par de scripts

---
+++ Assets/Scripts/Inicializador.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Aca va la conexion al server y la asignacion del turnos
+
+public class Inicializador : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
